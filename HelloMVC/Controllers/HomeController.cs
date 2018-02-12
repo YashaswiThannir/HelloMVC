037b22f1f2090b17c7fb851a5f9dcc779161f1cd
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2018-02-11 19:09:46 -0800

    MVC ASP .NET Learning

---
+++ HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,149 @@
+﻿using HelloMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //internal list
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        //creating an interface for our controller
+        public HomeController()
+        {
+            customers = cache["customers"] as List<Customer>; //telling the cache to store the list of customers.
+            if (customers == null)
+            {
+                customers = new List<Customer>();
+            }
+        }
+        public void SaveCache()
+        {
+            cache["customers"] = customers;
+        }
+        public ActionResult Index()//default page
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ViewCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(customer);
+            }
+            customer.Id = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+            return RedirectToAction("CustomerList");
+        }
+
+        public ActionResult EditCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditCustomer(Customer customer,string Id)
+        {
+            var customerToEdit = customers.FirstOrDefault(c => c.Id == Id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                customerToEdit.Name = customer.Name;
+                customerToEdit.Telephone = customer.Telephone;
+                SaveCache();
+                return RedirectToAction("CustomerList");
+            }
+        }
+
+        public ActionResult DeleteCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("DeleteCustomer")]
+        public ActionResult ConfirmDeleteCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                customers.Remove(customer);
+                return RedirectToAction("CustomerList");
+            }
+        }
+        public ActionResult CustomerList()
+        {
+            return View(customers);
+        }
+
+        public PartialViewResult Basket()
+        {
+            BasketViewModel model = new BasketViewModel();
+            model.BasketCount = 5;
+            model.BasketTotal = "$100";
+
+            return PartialView(model);
+        }
+    }
+}
